c59c05f4296e87237fc150e3d3a44bfccc1210e4
Maxim Urschumzew <u.maxim@live.de>
2016-09-27 00:01:10 +0200

    Add simple syntax tagger

---
+++ FatalHaskell/FatalHaskell.ProjectType/Editor/Syntax/SyntaxTagger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.Linq;
+
+namespace FatalHaskell.Editor
+{
+    class SyntaxTagger : ITagger<ClassificationTag>
+    {
+        ITextBuffer _buffer;
+        IClassificationTypeRegistryService ClassificationTypeRegistryService;
+
+        internal SyntaxTagger(ITextBuffer buffer, IClassificationTypeRegistryService regService)
+        {
+            _buffer = buffer;
+            ClassificationTypeRegistryService = regService;
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<ClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+
+            foreach (SnapshotSpan curSpan in spans)
+            {
+                var start = curSpan.Start;
+                var end = curSpan.End;
+                
+
+                while (start != end)
+                {
+                    var startWord = SkipWhile(start, end, Char.IsWhiteSpace);
+                    var endWord = SkipWhile(startWord, end, c => !Char.IsWhiteSpace(c));
+                    start = endWord;
+
+                    if (startWord != endWord)
+                    {
+                        var wordSpan = new SnapshotSpan(startWord, endWord);
+                        if (Char.IsUpper(wordSpan.GetText().First()))
+                        {
+                            yield return new TagSpan<ClassificationTag>(wordSpan,
+                                new ClassificationTag(ClassificationTypeRegistryService.GetClassificationType("string")));
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+
+        private static SnapshotPoint SkipWhile(SnapshotPoint p, SnapshotPoint end, Func<char, bool> f)
+        {
+            SnapshotPoint t = p;
+
+            while (t != end && f(t.GetChar()))
+            {
+                t += 1;
+            }
+            return t;
+        }
+    }
+}
---
+++ FatalHaskell/FatalHaskell.ProjectType/Editor/Syntax/SyntaxTaggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.ProjectSystem;
+
+namespace FatalHaskell.Editor
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("hs")]
+    [AppliesTo("FatalHaskell")]
+    [TagType(typeof(ClassificationTag))]
+    class SyntaxTaggerProvider : ITaggerProvider
+    {
+
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationTypeRegistryService { get; set; }
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return new SyntaxTagger(buffer, ClassificationTypeRegistryService) as ITagger<T>;
+        }
+    }
+}
